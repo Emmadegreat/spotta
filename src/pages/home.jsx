11d07b3ca144845a91bf0d7e6f36afd283ed5fcb
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-04-04 02:11:07 +0100

    add

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -27,6 +27,36 @@ const Home = () => {
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eos doloremque, harum illo dignissimos possimus. Sint ex, vitae sapiente voluptatibus iste voluptatem dicta suscipit illo, quasi quae sed amet temporibus.
                 </p>
+                <div className='card'>
+                    <div className='card-header'>
+                        <div className='card-right'>
+                            <img src="" alt="user" />
+                            <h4>James T.</h4>
+                            <span>5 months ago</span>
+                        </div>
+                        <div className='card-left'>
+                            <h4>Ikate, Lekki</h4>
+                            <span>star</span>
+                        </div>
+                    </div>
+
+                    <div className="card-body">
+                        <p>
+                            There is no stable electricity. The roads are fairly good and
+                            there is a sense of community. The drainage system is poor and
+                            most residents litter their surroundings. There are lots of
+                            stores and supermarkets.
+                        </p>
+                    </div>
+
+                    <div className="card-footer">
+                        <div>
+                            <div className="div"></div>
+                            <div className="div"><button>power</button></div>
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
 
